66f23fa61507c5a1a8e19549c0b36b4edcff6ee5
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-09-09 15:23:55 +0200

    Added some more hints for mc form

--- assets/scripts/routes/MC/Mailchimp.ts
+++ assets/scripts/routes/MC/Mailchimp.ts
@@ -20,13 +20,23 @@ export default class Mailchimp {
     stopEvents() {
         this.form!.addEventListener('submit', function (e: any) {
             e.preventDefault();
-
+            e.target.querySelector('button').innerHTML = 'Verzenden...';
+            e.target.querySelector('button').disabled = true;
+            
+            
             let data = new FormData(e.target);
-
+            
             fetch('/wp-json/djc/v1/mailchimp', {
                 method: 'POST',
                 body: data
-            }).then(res => console.dir(res));
+            }).then(res => {
+                e.target.reset();
+                e.target.querySelector('button').innerHTML = 'Aanmelden';
+                e.querySelector('button').disabled = false;
+            }).catch(err => {
+                e.target.querySelector('button').innerHTML = 'Aanmelden';
+                e.querySelector('button').disabled = false;
+            })
         })
     }
 }
