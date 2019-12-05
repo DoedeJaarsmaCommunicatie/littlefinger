f73be969f342c8d9f6b667753cb80a8c4c5008e0
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-12-05 11:26:25 +0100

    added opener for footer long lists
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- assets/scripts/routes/common.ts
+++ assets/scripts/routes/common.ts
@@ -12,8 +12,20 @@ export default {
         new NavOpener();
         new Mailchimp();
         FilterOpener();
+        openShortList();
     },
     finalize() {
         // Fires on all pages after page specific JS is loaded.
+    }
+}
+
+const openShortList = () => {
+    const lists = document.querySelectorAll('.shortened-list');
+    if (!lists || lists.length === 0) {
+        return;
+    }
+
+    for ( let i = 0; i < lists.length; i++ ) {
+        lists[i].addEventListener('click', () => lists[i].classList.add('active'));
     }
 }
