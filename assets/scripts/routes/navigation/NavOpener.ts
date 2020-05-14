79f4ec9ae0997006fe1fb0b4e16dd7973d163ca5
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-05-14 16:51:31 +0200

    phase 3.1

--- assets/scripts/routes/navigation/NavOpener.ts
+++ assets/scripts/routes/navigation/NavOpener.ts
@@ -15,9 +15,15 @@ export default class NavOpener {
         }
 
         button.addEventListener('click',  () => {
-            if (!this.menu.classList.contains('active')) {
+            this.setMobileMenuTop(this.menu);
+            if (this.menu.classList.contains('active')) {
+                this.menu.classList.remove('active');
+                document.body.style.overflow = '';
+            } else {
                 this.menu.classList.add('active');
+                document.body.style.overflow = 'hidden';
             }
+
         });
 
         document.body.addEventListener('click', (ev: any) => {
@@ -31,4 +37,31 @@ export default class NavOpener {
             }
         });
     }
+
+    setMobileMenuTop(menu: any): void {
+        const header = document.querySelector('.logo-header'),
+            topper = document.querySelector('header.header')!,
+            offset = header!.getBoundingClientRect().bottom - document.body.getBoundingClientRect().top;
+
+        if (!this.isOutOfViewport(header).any) {
+            menu.style.top = (offset - 2) + 'px';
+        } else {
+            menu.style.top = (topper.clientHeight) + 'px';
+        }
+    }
+
+    isOutOfViewport(elem: any) {
+        const bounding = elem.getBoundingClientRect(),
+            out = { top: false, left: false, bottom: false, right: false, any: false, all: false }
+
+        out.top = bounding.top < 0;
+        out.left = bounding.left < 0;
+        out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+        out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+        out.any = out.top || out.left || out.bottom || out.right;
+        out.all = out.top && out.left && out.bottom && out.right;
+
+        return out;
+
+    };
 }
--- src/Controllers/Meta/Blocks/Producten.php
+++ src/Controllers/Meta/Blocks/Producten.php
@@ -11,7 +11,7 @@ use Timber\Timber;
 class Producten extends Block
 {
     const DEFAULT = '\\Elderbraum\\CasaProductFactory\\Products\\Red';
-    
+
     private static $options = [
         '\\Elderbraum\\CasaProductFactory\\Products\\Red'          => 'Rode wijnen',
         '\\Elderbraum\\CasaProductFactory\\Products\\White'        => 'Witte wijnen',
@@ -19,9 +19,9 @@ class Producten extends Block
         '\\Elderbraum\\CasaProductFactory\\Products\\Mousserend'   => 'Mousserende wijnen',
         '\\Elderbraum\\CasaProductFactory\\Products\\Dessert'      => 'Dessert wijnen',
     ];
-    
+
     protected $template = 'partials/blocks/products.twig';
-    
+
     public function register(): void
     {
         $this->block()::make(__('Producten'))
@@ -30,7 +30,7 @@ class Producten extends Block
                       ->set_description('Een block met producten naast elkaar.')
                       ->set_render_callback([$this, 'render']);
     }
-    
+
     /**
      * @param $fields
      * @param $attributes
@@ -41,73 +41,73 @@ class Producten extends Block
     public function render($fields, $attributes, $inner_blocks)
     {
         $context = Timber::get_context();
-        
+
         /** @var \Elderbraum\CasaProductFactory\Products\Product $product */
         $product = ProductsFactory::create($this->getCategory($fields));
-        
+
         $context ['posts'] = Timber::get_posts($product->boot()->limit((int) ($fields['limit'] ?? 10))->get_args());
-        
+
         if ($this->isLargeSite()) {
             $context['link'] = $fields['link'];
         }
-        
+
         $context['attributes'] = $attributes;
         $context['title'] = $fields['title'] ?? false;
         $context['kiyoh'] =  $this->getKiyoh($fields);
-        
+
         return Timber::render($this->template, $context);
     }
-    
+
     public function fields(): array
     {
         $fields = [];
         $fields [] = Field::make('select', 'category', __('Category'))
             ->set_options(self::$options);
-        
+
         $fields [] = Field::make('text', 'title', __('Title'));
-        
+
         if ($this->isLargeSite()) {
             $fields [] = Field::make('text', 'link', __('Link'));
         }
-        
+
         $fields [] = Field::make('text', 'limit', __('Limit'))
                           ->set_attribute('type', 'number')
                           ->set_attribute('min', 0)
                           ->set_attribute('max', 20);
-        
+
         if ($this->isLargeSite()) {
             $fields [] = Field::make('checkbox', 'show_kiyoh', __('Kiyoh'));
         }
-        
-        
+
+
         return $fields;
     }
-    
+
     private function getCategory($fields): string
     {
         if (!isset($fields['category']) || $fields['category'] === '') {
             return self::DEFAULT;
         }
-        
+
         if (!array_key_exists($fields['category'], self::$options)) {
             return self::DEFAULT;
         }
-        
+
         return $fields[ 'category' ];
     }
-    
+
     private function getKiyoh(array $fields)
     {
-        if ($fields['show_kiyoh']) {
+        if (isset($fields['show_kiyoh'])) {
             if (true === get_theme_mod('cdelk_use_hash') && class_exists('cdk_hashed_model')) {
                 return  (new cdk_hashed_model())->get();
             } elseif (class_exists('cdk_model')) {
                 return (new cdk_model())->get();
             }
-            
+
             return false;
         }
-        
+
         return false;
     }
 }
