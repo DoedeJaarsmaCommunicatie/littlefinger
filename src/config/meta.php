ccc6d2f83b9c2aa14a26a5cdeeae2206237eb91e
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-06-23 08:31:16 +0200

    Removed product theme options

--- src/config/meta.php
+++ src/config/meta.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace App;
 
 use App\Controllers\Meta\Blocks\ImageProducts;
 use App\Controllers\Meta\Blocks\Producten;
 use App\Controllers\Meta\Blocks\ProductList;
 use App\Controllers\Meta\Fields\FrontPage;
+use App\Controllers\Meta\Fields\Products as ThemeOptionsProducts;
 use App\Controllers\Meta\MenuMeta;
 use App\Controllers\Meta\Options\Theme;
 
 return [
-	MenuMeta::class,
-	Theme::class,
-	FrontPage::class,
-	Producten::class,
-	ImageProducts::class,
-	ProductList::class,
+    MenuMeta::class,
+    Theme::class,
+    FrontPage::class,
+    Producten::class,
+    ImageProducts::class,
+    ProductList::class,
+    ThemeOptionsProducts::class
 ];
